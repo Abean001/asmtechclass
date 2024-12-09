b93cfd0d3c602794a808503adc3dac0e57c32758
Adrian - Abean001 <adrian@abean.cc>
2024-12-09 10:00:58 +0100

    Added Shit - 12/09/24 1000

---
+++ StoryTime.py
@@ -0,0 +1,9 @@
+message = """Dear Reader,
+The Following contains Horror Content Reader discretion advised
+
+Horror Story--
+
+I reached out to the zombie ready to slap its face.
+I reared back as a flash of pain went through me.
+I reared back as a flash of pain went through me.
+"""
